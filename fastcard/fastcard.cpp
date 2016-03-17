d75bc7b012360899ac7ad553f58a0400710b3155
Schalk-Willem Krüger <swk@swk.za.net>
2016-03-17 16:06:10 +0200

    fastcard: detect_carrier

--- fastcard/fastcard.cpp
+++ fastcard/fastcard.cpp
@@ -7,11 +7,18 @@
 #include <string.h>
 #include <complex>
 
+#define USE_VOLK
+
 #define FFTLIB FFTW
+
 #if FFTLIB == FFTW
 #include <fftw3.h>
 #else
 #error "Unknown FFT library"
+#endif
+
+#ifdef USE_VOLK
+#include <volk/volk.h>
 #endif
 
 typedef std::complex<float> fc_complex;
@@ -20,6 +27,11 @@ typedef std::complex<float> fc_complex;
 int block_size_log2N = 13; // 8196
 int history_size = 2085;
 
+float threshold_constant = 5;
+float threshold_snr = 3;
+int carrier_freq_min = 123;
+int carrier_freq_max = 130;
+
 // Internal
 int block_size = 1<<block_size_log2N;
 
@@ -27,6 +39,7 @@ int block_size = 1<<block_size_log2N;
 uint16_t *raw_samples;
 fc_complex *samples;
 fc_complex *fft;
+float *fft_mag;
 fc_complex lut[0x10000];
 
 void generate_lut() {
@@ -50,6 +63,10 @@ void free_fft();
 void init_buffers() {
     raw_samples = (uint16_t*) calloc(block_size, sizeof(uint16_t));
 
+    // size_t alignment = volk_get_alignment();
+    // fft_mag = (fc_complex*) volk_malloc(block_size * sizeof(fc_complex), alignment);
+    fft_mag = (float*) malloc(block_size * sizeof(float));
+
     if (raw_samples == NULL) {
         fprintf(stderr, "init buffers failed\n");
         exit(1);
@@ -63,6 +80,7 @@ void init_buffers() {
 void free_buffers() {
     free_fft();
     free(raw_samples);
+    free(fft_mag);
 }
 
 bool read_next_block(FILE *f) {
@@ -143,6 +161,53 @@ void free_fft() {
 
 #endif
 
+bool detect_carrier() {
+    // calculate magnitude
+#ifdef USE_VOLK
+    volk_32fc_magnitude_32f_u(fft_mag, fft, block_size);
+
+    float sum; // todo: volk_malloc
+    volk_32f_accumulator_s32f(&sum, fft_mag, block_size);
+
+    unsigned int argmax; // todo: volk_malloc
+    volk_32f_index_max_16u(
+            &argmax,
+            fft_mag + carrier_freq_min,
+            carrier_freq_max - carrier_freq_min + 1);
+    argmax += carrier_freq_min;
+    float max = fft_mag[argmax];
+
+#else
+    for (int i = 0; i < block_size; ++i) {
+        fft_mag[i] = std::abs(fft[i]);
+    }
+
+    float sum = 0;
+    for (int i = 0; i < block_size; ++i) {
+        sum += fft_mag[i];
+    }
+
+    float max = 0;
+    int argmax;
+    for (int i = carrier_freq_min; i <= carrier_freq_max; ++i) {
+        if (fft_mag[i] > max) {
+            argmax = i;
+            max = fft_mag[i];
+        }
+    }
+#endif
+
+    float mean = sum / block_size;
+    float threshold = threshold_constant + threshold_snr * mean;
+
+    if (max > threshold) {
+        fprintf(stderr, "detect @ mag[%d] = %f (thresh = %f)\n", argmax, max, threshold);
+        return true;
+    }
+
+    return false;
+}
+
 int main() {
     // FILE* in = stdin;
     FILE* in = fopen("test.dat", "rb");
@@ -156,6 +221,9 @@ int main() {
     while (read_next_block(in)) {
         convert_raw_to_complex();
         perform_fft();
+        if (detect_carrier()) {
+            // export
+        }
     }
 
     free_fft();
