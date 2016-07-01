4a0d078ccf25599d4208f8131caeae483e58d784
Schalk-Willem Krüger <swk@swk.za.net>
2016-07-01 14:29:48 +0200

    clock_sync.py: quick 'n dirty plot

--- clock_sync.py
+++ clock_sync.py
@@ -1,116 +1,143 @@
 #!/usr/bin/env python
 
-'''
+"""
 Build clock sync model from beacon transmissions.
-'''
+"""
 
-import argparse
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
 import data
 import matchmaker
-import sys
-from collections import deque
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-# TODO: support multiple beacons
-#  (we'll have to take RX and beacon positions as parameter)
-# TODO: support >2 receivers
+def plot(soa1, residuals, discontinuities, avg_snr=None):
+    s2m = 3e8 / 2.2e6  # FIXME
+
+    print("residuals: std dev = {:.01f} m; max = {:.01f} m; "
+          "avg corr snr = {:.01f}"
+          .format(np.std(residuals) * s2m,
+                  np.max(np.abs(residuals)) * s2m,
+                  avg_snr))
+
+    plt.figure(figsize=(11, 6))
+    plt.subplot(1, 2, 1)
+    plt.plot(soa1, residuals, '.-')
+    plt.title("Residuals")
+    plt.xlabel("RX sample")
+    plt.ylabel("Residual (samples)")
+    plt.grid()
+
+    plt.subplot(1, 2, 2)
+    plt.hist(residuals, 20)
+    plt.title("Histogram: residuals")
+    plt.grid()
+
+    plt.suptitle("Clock sync (stddev = {:.01f} m; max = {:.01f} m; "
+                 "avg corr SNR: {:.01f} dB)"
+                 .format(np.std(residuals) * s2m,
+                         np.max(np.abs(residuals)) * s2m,
+                         avg_snr))
+
+    plt.tight_layout()
+
+    plt.subplots_adjust(top=0.90)
+    # plt.savefig('clocksync.pdf', format='pdf')
+    plt.show()
+
+
 def clock_sync(detections, matches, beacon):
-    '''
-    Args:
-        detections: detection array
-        matches: match array
-        beacon: beacon ID
-    '''
-    num_rx = max([x.rxid for x in toads]) + 1
+    """
+    Prameters
+    ---------
+    detections: detection array
+    matches:    match array
+    beacon:     beacon ID
+    """
+
+    if len(matches) == 0:
+        return
+    num_rx = len(matches[0])
 
     # Extract beacon's matches
-    # We currently do not support incomplete matches
+    # Incomplete matches aren't supported at the moment
     matches = np.array([x for x in matches
-            if sum([y != -1 for y in x]) == num_rx and
-               detections['txid'][x[0]] == beacon])
-    print 'Number of detection groups:', len(matches)
+                       if sum([y != -1 for y in x]) == num_rx
+                        and detections['txid'][x[0]] == beacon])
+    print("Number of detection groups:", len(matches))
 
     soa = detections['soa'][matches]
-    sdoa = soa[:,1] - soa[:,0]
+    sdoa = soa[:, 1] - soa[:, 0]
     dsdoa = np.abs(np.diff(sdoa))
     is_rapid_change = dsdoa > np.mean(dsdoa) * 10
     discontinuities = np.where(is_rapid_change)[0]
 
     # print np.column_stack([np.diff(soa[:,0]), np.diff(soa[:,1]), sdoa[1:],
     #                        dsdoa, rapid_change]).astype(int)
-    print 'Number of discontinuities:', np.size(discontinuities)
-    print 'Discontinuities:', ' '.join(map(str, discontinuities))
+    print("Number of discontinuities:", np.size(discontinuities))
+    print("Discontinuities:", " ".join(map(str, discontinuities)))
+
+    all_soa1 = []
+    all_residuals = []
+    all_avg_snr = []
+    all_coefs = []
 
     edges = np.concatenate([[0], discontinuities, [len(matches)]])
     for i in range(len(edges) - 1):
-        a, b = edges[i] + 1, edges[i + 1]
-        if a + 3 >= b:
+        left, right = edges[i] + 1, edges[i + 1]
+        if left + 3 >= right:
             continue
-        # print 'Range', a, b
-        sync(soa[a:b])
+
+        soa_ab = soa[left:right]
+        coef, residuals = sync(soa_ab)
+
+        energy = detections['energy'][matches][left:right]
+        noise = detections['noise'][matches][left:right]
+        snr = 20 * np.log10(energy / noise)
+        avg_snr = np.mean(snr)
+
+        all_soa1.append(soa_ab[:, 0])
+        all_residuals.append(residuals)
+        all_avg_snr.append(avg_snr)
+        all_coefs.append(coef)
+
+    soa1 = np.concatenate(all_soa1)
+    residuals = np.concatenate(all_residuals)
+    avg_snr = np.mean(all_avg_snr)
+    plot(soa1, residuals, discontinuities, avg_snr)
+
+    return all_coefs
 
 
-    # # print detections['rxid'][matches]
-    # # print detections['txid']
-    # print sdoa
-
-    # detect discontinuities
-
-
-def sync(soa, deg=2, plot=False):
-    # sdoa = soa[:,1] - soa[:,0]
-    # dsdoa = np.abs(np.diff(sdoa))
-    # print np.column_stack([np.diff(soa[:,0]), np.diff(soa[:,1]), sdoa[1:],
-    #                        dsdoa]).astype(int)
-
-    soa1, soa2 = soa[:,0], soa[:,1]
+def sync(soa, deg=2):
+    soa1, soa2 = soa[:, 0], soa[:, 1]
     coef = np.polyfit(soa1, soa2, deg)
     fit = np.poly1d(coef)
     residuals = soa2 - fit(soa1)
 
-    s2m = 3e8 / 2.2e6
-    print 'residuals: std dev = {:.01f} m; max = {:.01f} m'.format(
-            np.std(residuals) * s2m,
-            np.max(np.abs(residuals)) * s2m)
+    return coef, residuals
 
-    # sdoa = soa2 - soa1
-    # dsdoa = np.concatenate([[0], np.diff(sdoa)])
-    # dsoa1 = np.concatenate([[0, 0], np.diff(np.diff(soa1))])
-    # dsoa2 = np.concatenate([[0, 0], np.diff(np.diff(soa2))])
-    # print np.column_stack([soa1, soa2, dsoa1, dsoa2, dsdoa, residuals*1000]).astype('int')
 
-    if plot:
-        plt.figure(figsize=(11,5))
-        plt.subplot(1,2,1)
-        plt.plot(soa1, residuals, '.-')
-        plt.title('residuals')
-        plt.xlabel('RX1 sample')
-        plt.ylabel('Residual (samples)')
-        plt.grid()
+def main():
+    import argparse
 
-        plt.subplot(1,2,2)
-        plt.hist(residuals, 20)
-        plt.title('Histogram: residuals')
-        plt.grid()
-        # plt.savefig('clock_sync_{}_captured.pdf'.format(basename, int(soa1[0])), format='pdf')
-        plt.show()
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-            description=__doc__,
-            formatter_class=argparse.RawDescriptionHelpFormatter)
+    formatter = argparse.RawDescriptionHelpFormatter
+    parser = argparse.ArgumentParser(description=__doc__,
+                                     formatter_class=formatter)
 
     parser.add_argument('toads',
                         type=argparse.FileType('rb'), default='rx.toads',
-                        help='toads data (\'-\' streams from stdin)')
+                        help="toads data (\"-\" streams from stdin)")
     parser.add_argument('matches',
                         type=argparse.FileType('rb'), default='rx.match',
-                        help='toads data (\'-\' streams from stdin)')
+                        help="toads data (\"-\" streams from stdin)")
     parser.add_argument('--id', dest='beacon_id', type=int, default=0,
-                        help='transmitter ID of beacon')
+                        help="transmitter ID of beacon")
 
     args = parser.parse_args()
 
@@ -118,6 +145,7 @@ if __name__ == '__main__':
     detections = data.toads_array(toads, with_ids=True)
     matches = matchmaker.load_matches(args.matches)
 
-    np.set_printoptions(threshold=np.nan) # FIXME
     clock_sync(detections, matches, args.beacon_id)
 
+if __name__ == '__main__':
+    main()
