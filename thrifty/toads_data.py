ddde731dae489270b6cb6236b9c88dd94a8073a8
Schalk-Willem Krüger <swk@swk.za.net>
2016-07-12 15:36:00 +0200

    Copy and clean up toads_data.py

---
+++ thrifty/toads_data.py
@@ -0,0 +1,124 @@
+"""Data structures and utility functions for working with .toad(s) files."""
+
+from collections import namedtuple
+
+import numpy as np
+
+
+CarrierSyncInfo = namedtuple('CarrierSyncInfo', [
+    'bin',
+    'offset',
+    'energy',
+    'noise'])
+
+
+CorrDetectionInfo = namedtuple('CorrDetectionInfo', [
+    'sample',
+    'offset',
+    'energy',
+    'noise'])
+
+
+class DetectionResult(object):
+    """Collection of information stored in .toad(s) files."""
+    def __init__(self, timestamp, block, soa, carrier_info, corr_info,
+                 rxid=None, txid=None):
+        """
+        Parameters
+        ----------
+        timestamp: float
+            Approximate time at which block has been captured.
+        block: int
+            Block index.
+        soa: int
+            Sample-of-arrival relative to the receiver's start time.
+        carrier_info: CarrierSyncResult
+        corr_info: CorrDetectionResult
+        """
+        # pylint: disable=too-many-arguments
+        self.timestamp = timestamp
+        self.block = block
+        self.soa = soa
+        self.carrier_info = carrier_info
+        self.corr_info = corr_info
+        self.rxid = rxid
+        self.txid = txid
+
+    def serialize(self):
+        """Serialize detection as a string written to a .toad(s) file."""
+        # pylint: disable=invalid-name
+        corr, carr = self.corr_info, self.carrier_info
+        s = '{t:.6f} {b} {s} {ps} {po} {pe} {pn} {cb} {co} {ce} {cn}'.format(
+            t=self.timestamp, b=self.block, s=self.soa,
+            ps=corr.sample, po=corr.offset, pe=corr.energy, pn=corr.noise,
+            cb=carr.bin, co=carr.offset, ce=carr.energy, cn=carr.noise)
+        # Prepend TX and RX IDs
+        if self.txid is not None:
+            s = str(self.txid) + ' ' + s
+        if self.rxid is not None:
+            s = str(self.rxid) + ' ' + s
+        return s
+
+    @classmethod
+    def deserialize(cls, string, with_rxid=False, with_txid=False):
+        """Deserialize detection from string read from .toad(s) file."""
+        # pylint: disable=invalid-name,too-many-locals
+        fields = string.split()
+
+        # Strip RX ID and TX ID
+        rxid = int(fields.pop(0)) if with_rxid else None
+        txid = int(fields.pop(0)) if with_txid else None
+
+        t, b, s, ps, po, pe, pn, cb, co, ce, cn = map(float, fields)
+
+        timestamp, block, soa = t, int(b), float(s)
+        toa = CorrDetectionInfo(sample=int(ps), offset=po, energy=pe, noise=pn)
+        carrier = CarrierSyncInfo(bin=int(cb), offset=co, energy=ce, noise=cn)
+
+        return DetectionResult(timestamp, block, soa, carrier, toa, rxid, txid)
+
+
+def _load_toads(stream, with_rxid=True, with_txid=True):
+    """Load list of TOA detection serializations from a file."""
+    toads = []
+    for line in stream:
+        if len(line) == 0 or line[0] == '#':
+            continue
+        detection = DetectionResult.deserialize(line,
+                                                with_rxid=with_rxid,
+                                                with_txid=with_txid)
+        toads.append(detection)
+    return toads
+
+
+def load_toad(stream):
+    """Load a single receiver's detection data from a .toad file."""
+    return _load_toads(stream, False, False)
+
+
+def load_toads(stream):
+    """Load multiple receivers' detection data from a .toads file."""
+    return _load_toads(stream, True, True)
+
+
+def toads_array(detections, with_ids=True):
+    """Create a structured nparray from an array of `DetectionResult`s."""
+    data = [
+        (
+            i, line.rxid if with_ids else -1, line.txid if with_ids else -1,
+            line.timestamp, line.block, line.soa,
+            line.toa.sample, line.toa.offset,
+            line.toa.energy, line.toa.noise,
+            line.carrier.bin, line.carrier.offset,
+            line.carrier.energy, line.carrier.noise
+        )
+        for i, line in enumerate(detections)
+    ]
+    return np.array(data, dtype=[
+        ('idx', 'i4'), ('rxid', 'i4'), ('txid', 'i4'),
+        ('timestamp', 'f8'), ('block', 'i4'), ('soa', 'f8'),
+        ('sample', 'i4'), ('offset', 'f8'),
+        ('energy', 'f8'), ('noise', 'f8'),
+        ('carrier_bin', 'i4'), ('carrier_offset', 'f8'),
+        ('carrier_energy', 'f8'), ('carrier_noise', 'f8')
+    ])
