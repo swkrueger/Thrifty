e89cb82ada841dd1d3c0ff7b567c9aaa2457381f
Schalk-Willem Krüger <swk@swk.za.net>
2016-09-22 13:50:50 +0200

    Add rpi detector service

---
+++ rpi/detect.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+shutdown() {
+    print "Shutdown"
+    kill $(jobs -p)
+    exit -1
+}
+
+trap "shutdown" SIGINT SIGTERM
+
+echo "Waiting for NTP sync..."
+ntp-wait
+
+echo "Starting Thrifty..."
+cd /home/pi/detector
+rm -f fifo.card buffer
+mkfifo fifo.card buffer
+TOAD_FILE="toad/$(date +"%Y-%m-%d_%H-%M-%S").toad"
+
+set -m
+(
+    (thrifty capture buffer >>capture.log || (echo "fastcard died prematurely" && kill 0)) &
+    (buffer -m 32M -s 55236 -i buffer -o fifo.card || (echo "buffer died prematurely" && kill 0)) &
+    (thrifty detect fifo.card -a $TOAD_FILE >>detect.log || (echo "thrifty detect died prematurely" && kill 0)) &
+    wait
+)
+
+exit 0
