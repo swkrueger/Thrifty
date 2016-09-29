8afc26dbb365a953940db3ce4d3394c2bdb4d220
Schalk-Willem Krüger <swk@swk.za.net>
2016-09-29 21:54:51 +0200

    clock_sync.py: quick'n'dirty mod with new .match format

--- thrifty/clock_sync.py
+++ thrifty/clock_sync.py
@@ -16,6 +16,42 @@ from thrifty import matchmaker
 from thrifty import toads_data
 
 
+def is_outlier(points, thresh=3.5):
+    """
+    Returns a boolean array with True if points are outliers and False
+    otherwise.
+
+    Copy pasta from https://stackoverflow.com/questions/11882393/
+
+    Parameters:
+    -----------
+        points : An numobservations by numdimensions array of observations
+        thresh : The modified z-score to use as a threshold. Observations with
+            a modified z-score (based on the median absolute deviation) greater
+            than this value will be classified as outliers.
+
+    Returns:
+    --------
+        mask : A numobservations-length boolean array.
+
+    References:
+    ----------
+        Boris Iglewicz and David Hoaglin (1993), "Volume 16: How to Detect and
+        Handle Outliers", The ASQC Basic References in Quality Control:
+        Statistical Techniques, Edward F. Mykytka, Ph.D., Editor.
+    """
+    if len(points.shape) == 1:
+        points = points[:, None]
+    median = np.median(points, axis=0)
+    diff = np.sum((points - median)**2, axis=-1)
+    diff = np.sqrt(diff)
+    med_abs_deviation = np.median(diff)
+
+    modified_z_score = 0.6745 * diff / med_abs_deviation
+
+    return modified_z_score > thresh
+
+
 def plot(soa1, residuals, discontinuities, avg_snr=None):
     s2m = 3e8 / 2.4e6  # FIXME
 
@@ -32,13 +68,17 @@ def plot(soa1, residuals, discontinuities, avg_snr=None):
     plt.xlabel("RX sample")
     plt.ylabel("Residual (samples)")
     plt.grid()
+    plt.ylim([-0.5, 0.5])
+
+    for discontinuity in discontinuities:
+        # print(discontinuity)
+        plt.axvline(discontinuity, color='k')
+    # TODO: plot discontinuities
 
     plt.subplot(1, 2, 2)
     plt.hist(residuals, 20)
     plt.title("Histogram: residuals")
     plt.grid()
-
-    # TODO: plot discontinuities
 
     plt.suptitle("Clock sync (stddev = {:.01f} m; max = {:.01f} m; "
                  "avg corr SNR: {:.01f} dB)"
@@ -51,10 +91,9 @@ def plot(soa1, residuals, discontinuities, avg_snr=None):
     plt.subplots_adjust(top=0.90)
     plt.savefig('clocksync.pdf', format='pdf')
     plt.savefig('clocksync.png', format='png')
-    plt.show()
 
 
-def clock_sync(detections, matches, beacon):
+def clock_sync(toads, all_matches, rx0, rx1, beacon):
     """
     Parameters
     ---------
@@ -63,22 +102,43 @@ def clock_sync(detections, matches, beacon):
     beacon:     beacon ID
     """
 
-    if len(matches) == 0:
-        return
-    num_rx = len(matches[0])
+    # TODO: refactor function prototype
+
+    matches = matchmaker.extract_match_matrix(toads, all_matches,
+                                              [rx0, rx1], [beacon])
+    matches = np.array(matches)
 
     # Extract beacon's matches
-    # Incomplete matches aren't supported at the moment
-    matches = np.array([x for x in matches
-                        if sum([y != -1 for y in x]) == num_rx
-                        and detections['txid'][x[0]] == beacon])
+    # np.array([x for x in matches
+    #                     if sum([y != -1 for y in x]) == num_rx
+    #                     and detections['txid'][x[0]] == beacon])
     print("Number of detection groups:", len(matches))
+
+    detections = toads_data.toads_array(toads, with_ids=True)
+
+    # rxid = detections['rxid'][matches]
+    # for a, b in rxid:
+    #     print(a, b)
+    # sys.exit(0)
 
     soa = detections['soa'][matches]
     sdoa = soa[:, 1] - soa[:, 0]
     dsdoa = np.abs(np.diff(sdoa))
+    dsdoa2 = np.diff(sdoa)
+    # plt.plot(dsdoa)
+    # plt.show()
     is_rapid_change = dsdoa > np.mean(dsdoa) * 10
     discontinuities = np.where(is_rapid_change)[0]
+    # for i, x in enumerate(dsdoa):
+    #     if x > np.mean(dsdoa) * 10:
+    #         a, b = matches[i + 1], matches[i]
+    #         print(x, a, b)
+    #         print('-', sdoa[i + 1], sdoa[i], detections['soa'][a], detections['soa'][b])
+    #         print('+', detections['timestamp'][a] - detections['timestamp'][b])
+    # timestamps = detections['timestamp'][matches]
+    # asdf = np.diff(timestamps[:, 1] - timestamps[:, 0])
+    # plt.plot(asdf)
+    # plt.show()
 
     # print np.column_stack([np.diff(soa[:,0]), np.diff(soa[:,1]), sdoa[1:],
     #                        dsdoa, rapid_change]).astype(int)
@@ -89,30 +149,40 @@ def clock_sync(detections, matches, beacon):
     all_residuals = []
     all_avg_snr = []
     all_coefs = []
+    discontinuity_soas = []
 
     edges = np.concatenate([[0], discontinuities, [len(matches)]])
     for i in range(len(edges) - 1):
         left, right = edges[i] + 1, edges[i + 1]
-        if left + 3 >= right:
+        if left + 8 >= right:
             continue
 
         soa_ab = soa[left:right]
         coef, residuals = sync(soa_ab)
 
-        energy = detections['energy'][matches][left:right]
-        noise = detections['noise'][matches][left:right]
+        offset = detections['soa'][matches][left:right]
+        # print(np.diff(offset[:, 1] - offset[:, 0]))
+
+        outliers = is_outlier(residuals)
+
+        energy = detections['energy'][matches][left:right][~outliers]
+        noise = detections['noise'][matches][left:right][~outliers]
         snr = 20 * np.log10(energy / noise)
         avg_snr = np.mean(snr)
 
-        all_soa1.append(soa_ab[:, 0])
-        all_residuals.append(residuals)
+        all_soa1.append(soa_ab[:, 0][~outliers])
+        all_residuals.append(residuals[~outliers])
         all_avg_snr.append(avg_snr)
         all_coefs.append(coef)
+
+        if right != len(matches):
+            discontinuity_soas.append(soa[right, 0])
+            print(soa[right, 0], '\t', soa[right, 1], '\t', dsdoa2[right])
 
     soa1 = np.concatenate(all_soa1)
     residuals = np.concatenate(all_residuals)
     avg_snr = np.mean(all_avg_snr)
-    plot(soa1, residuals, discontinuities, avg_snr)
+    plot(soa1, residuals, discontinuity_soas, avg_snr)
 
     return all_coefs
 
@@ -139,7 +209,7 @@ def sync(soa, deg=2):
 #     relsoa = soa2 - ref
 #     np.set_printoptions(threshold=np.inf)
 #     print(np.column_stack([sdoa, ref, relsoa]))
-# 
+#
 #     plt.plot(relsoa)
 #     plt.savefig('reldist.pdf', format='pdf')
 #     plt.savefig('reldist.png', format='png')
@@ -158,10 +228,10 @@ def _main():
     #  - if one parameter: append ".toads" and ".match"
     #  - if two parameters: use the files being specified
     parser.add_argument('toads', nargs='?',
-                        type=argparse.FileType('rb'), default='rx.toads',
+                        type=argparse.FileType('rb'), default='data.toads',
                         help="toads data (\"-\" streams from stdin)")
     parser.add_argument('matches', nargs='?',
-                        type=argparse.FileType('rb'), default='rx.match',
+                        type=argparse.FileType('rb'), default='data.match',
                         help="toads data (\"-\" streams from stdin)")
     parser.add_argument('--id', dest='beacon_id', type=int, default=0,
                         help="transmitter ID of beacon")
@@ -169,11 +239,13 @@ def _main():
     args = parser.parse_args()
 
     toads = toads_data.load_toads(args.toads)
-    detections = toads_data.toads_array(toads, with_ids=True)
     matches = matchmaker.load_matches(args.matches)
 
-    clock_sync(detections, matches, args.beacon_id)
+    clock_sync(toads, matches, 0, 1, args.beacon_id)
+    clock_sync(toads, matches, 0, 2, args.beacon_id)
+    clock_sync(toads, matches, 1, 2, args.beacon_id)
     # reldist(coefs, detections, matches)
+    plt.show()
 
 if __name__ == '__main__':
     _main()
