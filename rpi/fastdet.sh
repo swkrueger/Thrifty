b3ee5a5737a3cb7df71be784e833c26a6b37740d
Schalk-Willem Krüger <swk@swk.za.net>
2016-11-02 10:58:19 +0200

    fastdet.sh: output each session to a separate log file; don't export card

--- rpi/fastdet.sh
+++ rpi/fastdet.sh
@@ -6,12 +6,16 @@ echo "Stop and sync NTP"
 sudo systemctl stop ntp
 sudo ntpd -gq
 # Do not start NTP while SDR is running
+# It can be started in the future, but ensure there won't be any sudden
+#  "jumps" in the timestamps.
 sleep 5
 
 echo "Starting fastdet"
 cd /home/pi/detector
+mkdir -p toad/ card/ log/
 TOAD_FILE="toad/$(date +"%Y-%m-%d_%H-%M-%S").toad"
 CARD_FILE="card/$(date +"%Y-%m-%d_%H-%M-%S").card"
+LOG_FILE="log/$(date +"%Y-%m-%d_%H-%M-%S").log"
 . ./fastdet.cfg
 CARD_ARG=
 if [ -n "${EXPORT_CARD}" ]; then
@@ -28,4 +32,4 @@ exec fastdet \
     -i rtlsdr \
     -f ${RTL_FREQ} \
     -g ${RTL_GAIN} \
-    -o ${TOAD_FILE} ${CARD_ARG} >> fastdet.log
+    -o ${TOAD_FILE} ${CARD_ARG} >> ${LOG_FILE}
